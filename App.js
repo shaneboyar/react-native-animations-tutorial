4c3d1da8ee571605ba0395fab40f61b646243ffd
Shane <shane.boyar@gmail.com>
2019-03-06 10:14:16 -0500

    update

--- App.js
+++ App.js
@@ -1,49 +1,68 @@
 import React, { Component } from "react";
-import {
-  Button,
-  StyleSheet,
-  UIManager,
-  LayoutAnimation,
-  Image,
-  View
-} from "react-native";
+import { Animated, Button, StyleSheet, Easing, View } from "react-native";
 import jake from "./jake.png";
 
-UIManager.setLayoutAnimationEnabledExperimental &&
-  UIManager.setLayoutAnimationEnabledExperimental(true);
+const AnimatedThing = Animated.createAnimatedComponent;
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { position: "flex-start" };
+
+    this._rotationAnimation = new Animated.Value(0);
+    this._rotationOffset = 0;
+
+    this.state = {
+      spinning: false
+    };
   }
 
-  changePosition = position => {
-    LayoutAnimation.spring();
-    this.setState({
-      position
+  startLoopAnimation = () => {
+    this._rotationAnimation.setOffset(this._rotationOffset);
+    Animated.loop(
+      Animated.timing(this._rotationAnimation, {
+        toValue: 1,
+        duration: 2000,
+        easing: Easing.linear
+      })
+    ).start();
+  };
+
+  stopLoopAnimation = () => {
+    this._rotationAnimation.stopAnimation(currentValue => {
+      this._rotationOffset = currentValue;
     });
+  };
+
+  toggleSpinning = () => {
+    this.setState({ spinning: !this.state.spinning }, () => {
+      this.state.spinning
+        ? this.startLoopAnimation()
+        : this.stopLoopAnimation();
+    });
+  };
+
+  getRotationAnimation = () => {
+    const rotate = this._rotationAnimation.interpolate({
+      inputRange: [0, 1],
+      outputRange: ["0deg", "360deg"]
+    });
+    return { rotate };
   };
 
   render() {
     return (
-      <View style={[styles.container, { justifyContent: this.state.position }]}>
-        <Image source={jake} />
+      <View style={[styles.container]}>
+        <Animated.Image
+          source={jake}
+          style={{ transform: [this.getRotationAnimation()] }}
+        />
         <View style={styles.buttonsContainer}>
           <Button
             style={styles.button}
-            title="Top"
-            onPress={() => this.changePosition("flex-start")}
-          />
-          <Button
-            style={styles.button}
-            title="Middle"
-            onPress={() => this.changePosition("center")}
-          />
-          <Button
-            style={styles.button}
-            title="Bottom"
-            onPress={() => this.changePosition("flex-end")}
+            title={
+              this.state.spinning ? "Turn Spinning Off" : "Turn Spinning On"
+            }
+            onPress={() => this.toggleSpinning()}
           />
         </View>
       </View>
