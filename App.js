68e58f684b0c66498cdbc2edb5814f4469af2f91
Shane <shane.boyar@gmail.com>
2019-03-07 17:12:54 -0500

    panhandlers

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
-import React, { Component } from "react";
-import { Animated, Button, StyleSheet, Easing, View } from "react-native";
-import jake from "./jake.png";
+import React, { Component } from 'react';
+import {
+  Animated,
+  Button,
+  StyleSheet,
+  Easing,
+  View,
+  PanResponder,
+} from 'react-native';
+import jake from './jake.png';
 
 export default class App extends Component {
   constructor(props) {
@@ -9,8 +16,49 @@ export default class App extends Component {
     this._rotationAnimation = new Animated.Value(0);
     this._rotationOffset = 0;
 
+    this._gestureOffset = { x: 0, y: 0 };
+    this._gestureValue = new Animated.ValueXY();
+    this._panResponder = PanResponder.create({
+      // Ask to be the responder:
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+
+      onPanResponderGrant: (evt, gestureState) => {
+        // The gesture has started. Show visual feedback so the user knows
+        // what is happening!
+        // gestureState.d{x,y} will be set to zero now
+      },
+      onPanResponderMove: (evt, gestureState) => {
+        // The most recent move distance is gestureState.move{X,Y}
+        // The accumulated gesture distance since becoming responder is
+        // gestureState.d{x,y}
+        this._gestureValue.setValue({
+          x: this._gestureOffset.x + gestureState.dx,
+          y: this._gestureOffset.y + gestureState.dy
+        });
+      },
+      onPanResponderTerminationRequest: (evt, gestureState) => true,
+      onPanResponderRelease: (evt, gestureState) => {
+        // The user has released all touches while this view is the
+        // responder. This typically means a gesture has succeeded
+        this._gestureOffset.x += gestureState.dx;
+        this._gestureOffset.y += gestureState.dy;
+      },
+      onPanResponderTerminate: (evt, gestureState) => {
+        // Another component has become the responder, so this gesture
+        // should be cancelled
+      },
+      onShouldBlockNativeResponder: (evt, gestureState) => {
+        // Returns whether this component should block native components from becoming the JS
+        // responder. Returns true by default. Is currently only supported on android.
+        return true;
+      },
+    });
+
     this.state = {
-      spinning: false
+      spinning: false,
     };
   }
 
@@ -20,7 +68,7 @@ export default class App extends Component {
       Animated.timing(this._rotationAnimation, {
         toValue: 1,
         duration: 2000,
-        easing: Easing.linear
+        easing: Easing.linear,
       })
     ).start();
   };
@@ -42,7 +90,7 @@ export default class App extends Component {
   getRotationAnimation = () => {
     const rotate = this._rotationAnimation.interpolate({
       inputRange: [0, 1],
-      outputRange: ["0deg", "360deg"]
+      outputRange: ['0deg', '360deg'],
     });
     return { rotate };
   };
@@ -52,13 +100,20 @@ export default class App extends Component {
       <View style={[styles.container]}>
         <Animated.Image
           source={jake}
-          style={{ transform: [this.getRotationAnimation()] }}
+          style={{
+            transform: [
+              this.getRotationAnimation(),
+              { translateX: this._gestureValue.x },
+              { translateY: this._gestureValue.y },
+            ],
+          }}
+          {...this._panResponder.panHandlers}
         />
         <View style={styles.buttonsContainer}>
           <Button
             style={styles.button}
             title={
-              this.state.spinning ? "Turn Spinning Off" : "Turn Spinning On"
+              this.state.spinning ? 'Turn Spinning Off' : 'Turn Spinning On'
             }
             onPress={() => this.toggleSpinning()}
           />
@@ -71,20 +126,20 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
     paddingTop: 64,
-    paddingBottom: 32
+    paddingBottom: 32,
   },
   buttonsContainer: {
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    position: "absolute",
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    position: 'absolute',
     bottom: 16,
-    width: "100%"
+    width: '100%',
   },
   button: {
-    width: 100
-  }
+    width: 100,
+  },
 });
